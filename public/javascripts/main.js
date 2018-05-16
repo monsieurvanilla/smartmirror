18fae0a6d8feff4d4051996679b0b1ad17a4399b
Stacey Mulcahy <stmulcah@microsoft.com>
2018-05-16 08:13:56 -0700

    styling for some of the items

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,7 @@
 Anytime you see the $ sign in JavaScript - that is a reference to the JQuery library. JQuery helps you do common things easier. 
 In this case, this "ready" call is a function that is called when the page has loaded and is ready -
 this is important cos you want to make sure everything is loaded and available before referencing it
-*/ 
+*/
 $(document).ready(function () {
 
     // can is a variable that will get the canvas element that has an ID of photo
@@ -26,29 +26,28 @@ $(document).ready(function () {
     // bad mix of vanilla and jquery. ooops
 
 
-    var emotionImages ={};
-    emotionImages["anger"]="anger.svg";
-    emotionImages["happiness"]="happiness.svg";
-    emotionImages["sadness"]="sadness.svg";
-    emotionImages["fear"]="fear.svg";
-    emotionImages["contempt"]="contempt.svg";
-    emotionImages["disgust"]="disgust.svg";
-    emotionImages["neutral"]="neutral.svg";
-    emotionImages["surprise"]="surprise.svg";
+    /* Emotion Images Object */
+        var emotionImages = {};
+        emotionImages["anger"] = "anger.svg";
+        emotionImages["happiness"] = "happiness.svg";
+        emotionImages["sadness"] = "sadness.svg";
+        emotionImages["fear"] = "fear.svg";
+        emotionImages["contempt"] = "contempt.svg";
+        emotionImages["disgust"] = "disgust.svg";
+        emotionImages["neutral"] = "neutral.svg";
+        emotionImages["surprise"] = "surprise.svg";
 
 
-    $('#sendbutton').click(function(eObject){
-        console.log("on Click yo")
-       //checkImage();
+    $('#sendbutton').click(function (eObject) {
+        console.log("click handlers");
+        //checkImage();
     })
 
-    function checkImage(){
+    function checkImage() {
         console.log("CheckImage called");
         can.toBlob(findOut);
         console.log("look at me")
     }
-
-
 
     can.style.width = window.innerWidth + "px";
     can.style.height = window.innerHeight + "px";
@@ -57,7 +56,7 @@ $(document).ready(function () {
         navigator.webkitGetUserMedia ||
         navigator.mozGetUserMedia ||
         navigator.msGetUserMedia);
-        // Hey, if there is getUserMedia and it returns turn, we're gonna ask for that video feed!
+    // Hey, if there is getUserMedia and it returns turn, we're gonna ask for that video feed!
     if (navigator.getUserMedia) {
         // Request access to video only
         navigator.getUserMedia(
@@ -79,7 +78,7 @@ $(document).ready(function () {
                 alert('Something went wrong. (error code ' + error.code + ')');
                 return;
             }
-            );
+        );
     }
     else {
         alert('Sorry, the browser you are using doesn\'t support getUserMedia');
@@ -94,78 +93,61 @@ $(document).ready(function () {
         have no data! */
         // detect all faces in photo and try to identify if any of them are willman
         detectFaces(photo)
-          .then((response) => response.json())
-          .then(function(response){
-            console.log("Hey this is our data",response);
-            processDataFromAPI(response);
+            .then((response) => response.json())
+            .then(function (response) {
+                console.log("Detected faces: response", response);
+                processDataFromAPI(response);
+            })
+    }
 
-          })
-      }
-
-      function processDataFromAPI(response){
-
-
+    function processDataFromAPI(response) {
         console.log("process data from api");
         console.log(response)
         var len = response.length;
-        console.log(len)
-    
-        for (var i=0;i<len;i++){
+        console.log(len);
+        for (var i = 0; i < len; i++) {
             var face = response[i];
             var faceAttributeObject = face['faceAttributes'];
             console.log(faceAttributeObject);
-            var emotionObject =  faceAttributeObject['emotion'];
+            var emotionObject = faceAttributeObject['emotion'];
             console.log(emotionObject);
             console.log("get the emottion");
             var mainEmotion = getHighestEmotion(emotionObject);
-            console.log("Main Emotion"+mainEmotion);
+            console.log("Main Emotion" + mainEmotion);
             showEmotion(mainEmotion);
-    
-           
         }
-      }
+    }
 
-
-      function showEmotion(mainEmotion){
-      
+    function showEmotion(mainEmotion) {
         $("#emotions").removeClass();
         $('#emotions').addClass(mainEmotion);
         $('#emotions').addClass('emotions');
+    }
 
-
+    /* 
+        Function to get the Emotion that rated the highest. 
         
-      }
-
-     function getHighestEmotion(emotion){
-         
-        var item = 'stacey';
-        var emotionValue =0;
-
-        for(var i in emotion){
-           
-            if(emotion[i]>emotionValue){
-             
+    */
+    function getHighestEmotion(emotion) {
+        var item = 'neutral';// just defaulting the variable to neutral 
+        var emotionValue = 0;
+        for (var i in emotion) {
+            if (emotion[i] > emotionValue) {
                 emotionValue = emotion[i];
                 item = i;
-
             }
         }
-
         return item;
-     }
-      
-  
-
-      function getGeneralScore(){
+    }
 
 
-      }
-    
-      /*
-    Function to check the Image - this will get the data image from the Canvas context and then convert it to a blob.
-    When that is done, it will call the "findOut" function
-    */
-    function checkImage(){
+
+
+    /*
+  Function to check the Image - this will get the data image from the Canvas context and then convert it to a blob.
+  When that is done, it will call the "findOut" function
+  */
+    function checkImage() {
         can.toBlob(findOut);
     }
 
@@ -174,21 +156,21 @@ $(document).ready(function () {
         console.log("show image")
         context.drawImage(v, 0, 0, can.width, can.height);
     }
-    function showTime(){
+    function showTime() {
         var currentTime = moment().format('LT');
         $(timeHolder).text(currentTime);
     }
 
-    function showItems(){
+    function showItems() {
         showTime();
-        showImage();
-        checkImage();
+        //showImage();
+        //checkImage();
     }
     setInterval(showItems, 10000);
-  
+
     showTime();
 
-    
+
     /*
     
     Function to detect Faces - we need photo data from the blob
@@ -197,62 +179,62 @@ $(document).ready(function () {
 
     function detectFaces(photo) {
         var options = {
-          method: "POST",
-          body: photo,
-          headers: {
-            "Content-Type": "application/octet-stream",
-            "Ocp-Apim-Subscription-Key": subscriptionKey
-          }
-      
+            method: "POST",
+            body: photo,
+            headers: {
+                "Content-Type": "application/octet-stream",
+                "Ocp-Apim-Subscription-Key": subscriptionKey
+            }
+
         };
         console.log(options);
         console.log(subscriptionKey)
         return fetch(detectUrl, options);
-      }
-
-      /*
-      Get Weather - Will call teh open weather map and get the weather. We then determine teh class name based on the
-      ID returned. If you want to make this icon big - you do it in style sheets. Treat it like a font - so you would do something like
-      font-size:48px for example. This is how you resize it. 
-      */
-function getWeather(){
-    $.get( "http://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=52023b5a67ea9a8811e25266368056b5", function( data ) {
-
-       console.log(data)
-       console.log(data.weather[0])
-       console.log(data.weather[0].icon)
-       console.log(getClassForWeather(data.weather[0].id,data.weather[0].icon));
-     $(weather).text(data.weather[0].description);
-       var weatherClassName = getClassForWeather(data.weather[0].id,data.weather[0].icon);
-      $(weathericon).removeClass();
-      $(weathericon).addClass("wi");
-      $(weathericon).addClass(weatherClassName);
-
-      });
-}
-getWeather();
-
-/*
-This function checks to see what class name to return to use for the icon, shouldn't need to be touched but coudl be improved!
-*/
-function getClassForWeather(id,icon){
-
-    var isNight = (icon.indexOf('n')>-1)?true:false;
-    var isDay = (icon.indexOf('d')>-1)?true:false;
-
-    var className="";
-    if(isNight && !isDay){
-        className = "wi-owm-night-";
-    }else if(isDay && !isNight){
-        className = "wi-owm-day-";
-    }else{
-        className = "wi-own-";
     }
-    className+=id;
-    console.log(className);
-    return className;
 
-}
+    /*
+    Get Weather - Will call teh open weather map and get the weather. We then determine teh class name based on the
+    ID returned. If you want to make this icon big - you do it in style sheets. Treat it like a font - so you would do something like
+    font-size:48px for example. This is how you resize it. 
+    */
+    function getWeather() {
+        $.get("http://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=52023b5a67ea9a8811e25266368056b5", function (data) {
+
+            console.log(data)
+            console.log(data.weather[0])
+            console.log(data.weather[0].icon)
+            console.log(getClassForWeather(data.weather[0].id, data.weather[0].icon));
+            $(weather).text(data.weather[0].description);
+            var weatherClassName = getClassForWeather(data.weather[0].id, data.weather[0].icon);
+            $(weathericon).removeClass();
+            $(weathericon).addClass("wi");
+            $(weathericon).addClass(weatherClassName);
+
+        });
+    }
+    getWeather();
+
+    /*
+    This function checks to see what class name to return to use for the icon, shouldn't need to be touched but coudl be improved!
+    */
+    function getClassForWeather(id, icon) {
+
+        var isNight = (icon.indexOf('n') > -1) ? true : false;
+        var isDay = (icon.indexOf('d') > -1) ? true : false;
+
+        var className = "";
+        if (isNight && !isDay) {
+            className = "wi-owm-night-";
+        } else if (isDay && !isNight) {
+            className = "wi-owm-day-";
+        } else {
+            className = "wi-own-";
+        }
+        className += id;
+        console.log(className);
+        return className;
+
+    }
 
 });
 
